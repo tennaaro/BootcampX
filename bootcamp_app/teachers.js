7e888ae4a90c911a3ecd7a48370c4e3f8e96f8cc
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-10-13 18:30:46 -0400

    added parametrized values

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,6 +7,28 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+/* '${process.argv[2] || 'JUL02'}' */
+
+
+const cohortName = process.argv[2] || 'JUL02';
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+  `;
+values = [cohortName]
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
+
+/*
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
@@ -21,3 +43,4 @@ ORDER BY teacher;
     console.log(`${row.cohort}: ${row.teacher}`);
   })
 });
+*/
